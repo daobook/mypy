fccce3d0b76022cf4cb0ba93d89f62d9ff38b2a1
xinetzone <xinetzone@users.noreply.github.com>
2022-03-04 12:08:58 +0000

    deploy: 7f0839b552c99f62636632bfd44522e91b26dc54

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.940+dev.d02f39d1f0317448dbbec1e7a5417a3780c278f2',
+    VERSION: '0.940+dev.7f0839b552c99f62636632bfd44522e91b26dc54',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
